9f5e2a5a686ea62d02c66bcffd563ab69e005926
black-adm <mattheuscontato17@gmail.com>
2025-11-30 18:56:24 -0300

    fix: tailwind setup and all assets config

---
+++ src/Syscode42.Blog/Program.cs
@@ -0,0 +1,24 @@
+using Syscode42.Blog.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapStaticAssets();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
