463ebedd1595067510cadce88b627ef2e13bcc59
black-adm <mattheuscontato17@gmail.com>
2025-12-01 09:00:02 -0300

    fix: static navigation links and update assets

---
+++ src/Syscode42.Blog/Models/PostFilters.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Syscode42.Blog.Models
+{
+    public enum PostFilterEnum
+    {
+		[Description("Todas Publicações")]
+		AllPosts,
+
+		[Description("Em Alta")]
+		Trending,
+
+		[Description("Mais Recentes")]
+		MostRecents,
+
+		[Description("Mais Antigos")]
+		OlderPosts,
+
+		[Description("Mais Curtidos")]
+		MostLiked
+	}
+}
